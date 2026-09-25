e5a4696f449b4ef82ce6052018c9cf2ee80a463d
Carlos La Fuente <carlos.aurelio.la.ca@gmail.com>
2026-09-24 22:04:40 -0400

    Agregando health

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
+
 app = FastAPI()
+
+
 @app.get("/")
 def root():
-    return {"message": "Sistema de inventario API"}
+    return {"message": "Inventory System API"}
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
